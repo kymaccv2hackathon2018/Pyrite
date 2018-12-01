2fc1ed2d66f6a0d85b3327954f63e583ff2909bb
Pablo Bles <pablo.bles@sap.com>
2018-11-30 17:06:31 -0700

    Initial spring boot app with Dockerfile

---
+++ cart-abandonment-service/src/main/java/com/pyrite/cartabandonmentservice/CartAbandonmentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.pyrite.cartabandonmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartAbandonmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartAbandonmentServiceApplication.class, args);
+	}
+}
