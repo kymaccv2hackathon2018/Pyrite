8df99833cb62a3068d9b6a46ca8a9fa4289cb631
John Raber <john.raber@sap.com>
2018-12-03 16:56:23 -0700

    added sap fundamental sytle and put grid tile in dashboard as temp

--- cart-visual/src/app/app.module.ts
+++ cart-visual/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { FundamentalNgxModule } from 'fundamental-ngx';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FundamentalNgxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
