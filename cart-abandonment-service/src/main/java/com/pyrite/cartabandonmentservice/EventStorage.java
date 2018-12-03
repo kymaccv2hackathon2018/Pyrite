48fc116ed867e6522286d74db920456a7b3e6862
Pablo Bles <pablo.bles@sap.com>
2018-12-03 14:29:42 -0700

    message handling

---
+++ cart-abandonment-service/src/main/java/com/pyrite/cartabandonmentservice/EventStorage.java
@@ -0,0 +1,41 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.pyrite.cartabandonmentservice;
+
+import static com.pyrite.cartabandonmentservice.CommerceProtos.ProductAddToCart;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventStorage
+{
+	private Map<String, List<ProductAddToCart>> addToCartEvents = new LinkedHashMap<>();
+
+
+	public void addToCartEvent(final String userId, final ProductAddToCart event)
+	{
+		if (addToCartEvents.containsKey(userId))
+		{
+			addToCartEvents.get(userId).add(event);
+		}
+		else
+		{
+			addToCartEvents.put(userId, Arrays.asList(event));
+		}
+
+	}
+}
--- cart-abandonment-service/src/main/java/com/pyrite/cartabandonmentservice/EventUtil.java
+++ cart-abandonment-service/src/main/java/com/pyrite/cartabandonmentservice/EventUtil.java
@@ -2,97 +2,113 @@ package com.pyrite.cartabandonmentservice;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
-import com.pyrite.cartabandonmentservice.CommerceProtos;
 
-public class EventUtil {
+public class EventUtil
+{
 
-    private static final JsonFormat.Printer printer = JsonFormat.printer().includingDefaultValueFields();
-    
-    /**
-     * Handler is a convenience interface to get typed event callbacks during a JSON.parse cycle.
-     */
-    public interface Handler {
-        void handleProductCreated(CommerceProtos.ProductCreated e);
-        void handleSiteCreated(CommerceProtos.SiteCreated e);
-        void handleCustomerCreated(CommerceProtos.CustomerCreated e);
-    }
+	private static final JsonFormat.Printer printer = JsonFormat.printer().includingDefaultValueFields();
 
-    /**
-     * Parse an eventlist json string and callback the events onto the given Handler interface.
-     * 
-     * @param json The raw json string
-     * @param handler The Handler callback interface
-     * @throws InvalidProtocolBufferException
-     */
-    public static void parseEventList(String json, Handler handler) throws InvalidProtocolBufferException {
-        CommerceProtos.EventList.Builder builder = CommerceProtos.EventList.newBuilder();
-        JsonFormat.parser().merge(json, builder);
+	/**
+	 * Handler is a convenience interface to get typed event callbacks during a JSON.parse cycle.
+	 */
+	public interface Handler
+	{
+		void handleProductCreated(CommerceProtos.ProductCreated e);
 
-        for (CommerceProtos.Event e : builder.build().getEventList()) {
-            handleEvent(e, handler);
-        }
-    }
+		void handleSiteCreated(CommerceProtos.SiteCreated e);
 
-    /**
-     * Parse an event json string and callback the event onto the given Handler interface.
-     * 
-     * @param json The raw json string
-     * @param handler The Handler callback interface
-     * @throws InvalidProtocolBufferException
-     */
-    public static void parseEvent(String json, Handler handler) throws InvalidProtocolBufferException {
-        CommerceProtos.Event.Builder builder = CommerceProtos.Event.newBuilder();
-        JsonFormat.parser().merge(json, builder);
-        handleEvent(builder.build(), handler);
-    }
+		void handleCustomerCreated(CommerceProtos.CustomerCreated e);
 
-    /**
-     * handleEvent switches on the event type and performs a callback on the given handler.
-     * 
-     * @param e
-     * @param handler
-     * @throws InvalidProtocolBufferException
-     */
-    public static void handleEvent(CommerceProtos.Event e, Handler handler) throws InvalidProtocolBufferException {
-        switch (e.getType()) {
-        case UNKNOWN:
-            break;
-        case CUSTOMER_CREATED:
-            handler.handleCustomerCreated(e.getCustomerCreated());
-            break;
-        case SITE_CREATED:
-            handler.handleSiteCreated(e.getSiteCreated());
-            break;
-        case PRODUCT_CREATED:
-           handler.handleProductCreated(e.getProductCreated());
-            break;
-        default:
-            throw new RuntimeException(String.format("Unknown event: %s", printer.print(e)));
-        }
-    }
+		void handleProductAddToCart(CommerceProtos.ProductAddToCart e);
+	}
 
-    /**
-     * Main entrypoint can be used to play around with json string parsing/formatting.
-     * 
-     * @param args
-     */
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.format("Error: raw json string representing an event list should be first argument\n");
-            System.err.format("Usage: $0 '{ \"event\": [...] }'\n");
-            System.exit(1);
-        }
+	/**
+	 * Parse an eventlist json string and callback the events onto the given Handler interface.
+	 *
+	 * @param json The raw json string
+	 * @param handler The Handler callback interface
+	 * @throws InvalidProtocolBufferException
+	 */
+	public static void parseEventList(String json, Handler handler) throws InvalidProtocolBufferException
+	{
+		CommerceProtos.EventList.Builder builder = CommerceProtos.EventList.newBuilder();
+		JsonFormat.parser().merge(json, builder);
 
-        String json_string = args[0];
-        CommerceProtos.EventList.Builder builder = CommerceProtos.EventList.newBuilder();
+		for (CommerceProtos.Event e : builder.build().getEventList())
+		{
+			handleEvent(e, handler);
+		}
+	}
 
-        try {
-            JsonFormat.parser().merge(json_string, builder);
-            System.out.format("%s\n", printer.print(builder));
-        } catch (InvalidProtocolBufferException ex) {
-            ex.printStackTrace();
-        }
+	/**
+	 * Parse an event json string and callback the event onto the given Handler interface.
+	 *
+	 * @param json The raw json string
+	 * @param handler The Handler callback interface
+	 * @throws InvalidProtocolBufferException
+	 */
+	public static void parseEvent(String json, Handler handler) throws InvalidProtocolBufferException
+	{
+		CommerceProtos.Event.Builder builder = CommerceProtos.Event.newBuilder();
+		JsonFormat.parser().merge(json, builder);
+		handleEvent(builder.build(), handler);
+	}
 
-    }
+	/**
+	 * handleEvent switches on the event type and performs a callback on the given handler.
+	 *
+	 * @param e
+	 * @param handler
+	 * @throws InvalidProtocolBufferException
+	 */
+	public static void handleEvent(CommerceProtos.Event e, Handler handler) throws InvalidProtocolBufferException
+	{
+		switch (e.getType())
+		{
+			case UNKNOWN:
+				break;
+			case CUSTOMER_CREATED:
+				handler.handleCustomerCreated(e.getCustomerCreated());
+				break;
+			case SITE_CREATED:
+				handler.handleSiteCreated(e.getSiteCreated());
+				break;
+			case PRODUCT_CREATED:
+				handler.handleProductCreated(e.getProductCreated());
+				break;
+			case PRODUCT_ADD_TO_CART:
+				handler.handleProductAddToCart(e.getProductAddToCart());
+				break;
+			default:
+				throw new RuntimeException(String.format("Unknown event: %s", printer.print(e)));
+		}
+	}
 
+	/**
+	 * Main entrypoint can be used to play around with json string parsing/formatting.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		if (args.length != 1)
+		{
+			System.err.format("Error: raw json string representing an event list should be first argument\n");
+			System.err.format("Usage: $0 '{ \"event\": [...] }'\n");
+			System.exit(1);
+		}
+
+		String json_string = args[0];
+		CommerceProtos.EventList.Builder builder = CommerceProtos.EventList.newBuilder();
+
+		try
+		{
+			JsonFormat.parser().merge(json_string, builder);
+			System.out.format("%s\n", printer.print(builder));
+		}
+		catch (InvalidProtocolBufferException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
 }
