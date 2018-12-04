d30d67973b4ce7b5a994e89091280e77c674fb1b
Paul Johnston <paul.johnston02@sap.com>
2018-12-04 15:49:07 -0700

    Team Pyrite Hackathon Winners!
    19 to 16

--- eventgen/serve.go
+++ eventgen/serve.go
@@ -16,6 +16,10 @@ import (
 	c "github.wdf.sap.corp/team-pyrite/hackathon/proto/commerce"
 )
 
+const Format = "2006-01-02T15:04:05"
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 var sendCommand = &cli.Command{
 	Name:  "send",
 	Usage: "send web content",
@@ -23,7 +27,7 @@ var sendCommand = &cli.Command{
 		&cli.StringFlag{
 			Name:   "endpoint",
 			Usage:  "Name of service to send events to",
-			Value:  "https://cart-abandonment-detector-stage.sa-hackathon-10.cluster.extend.sap.cx",
+			Value:  "https://event-ingress-stage.sa-hackathon-10.cluster.extend.sap.cx",
 			EnvVar: "EVENTGEN_ENDPOINT",
 		},
 		&cli.StringFlag{
@@ -47,6 +51,11 @@ var sendCommand = &cli.Command{
 			Name:  "dry_run",
 			Usage: "Don't actually send events",
 		},
+		&cli.IntFlag{
+			Name:  "tick_interval",
+			Usage: "Seconds interval for http posts",
+			Value: 2,
+		},
 	},
 	Action: func(c *cli.Context) error {
 		err := send(c)
@@ -60,67 +69,57 @@ var sendCommand = &cli.Command{
 func send(ctx *cli.Context) error {
 	endpoint := ctx.String("endpoint")
 	dryRun := ctx.Bool("dry_run")
+	tickInterval := ctx.Int("tick_interval")
 
 	// printEnvVars()
-	g := NewGenerator(endpoint, dryRun)
+	g := NewGenerator(endpoint, dryRun, tickInterval)
 
 	// Populate sites
-	bikes := g.createSite("1", "Nice Bikes")
+	bikes := g.createSite("Nice Bikes", "A Nice Bike Store")
 
 	// Populate products
-	frame := g.createProduct("1", "Carbon Fiber Frame")
-	wheels := g.createProduct("2", "Carbon Fiber Wheels")
-	seatPost := g.createProduct("3", "Carbin Fiber Seatpost")
+	frame := g.createProduct("Carbon-Fiber-Frame", "Canyon Bike Frame")
+	wheels := g.createProduct("Carbon-Fiber-Wheels", "Canyon Fiber Wheelset")
+	seatPost := g.createProduct("Carbon-Fiber-Seatpost", "Canyon Seatpost")
 
 	// Populate users
-	rafal := g.createCustomer("1", "Rafal", "rafal@example.com")
-	// yoni := g.createCustomer("2", "Yoni", "yoni@example.com")
-	// mike := g.createCustomer("3", "Mike", "mike@example.com")
+	rafal := g.createCustomer("Rafal", "rafal@example.com")
+	yoni := g.createCustomer("Yoni", "yoni@example.com")
+	mike := g.createCustomer("Mike", "mike@example.com")
 
 	// Populate carts
 	rafalCart := g.createCart(bikes, rafal, "1")
-	// yoniCart := g.createCart(bikes, yoni, "2")
-	// mikeCart := g.createCart(bikes, mike, "3")
+	yoniCart := g.createCart(bikes, yoni, "2")
+	mikeCart := g.createCart(bikes, mike, "3")
 
-	// Add products to carts
-	rafalFrame := g.addProductToCart(rafal, rafalCart, frame, now())
-	rafalWheels := g.addProductToCart(rafal, rafalCart, wheels, now())
-	rafalSeatPost := g.addProductToCart(rafal, rafalCart, seatPost, now())
+	g.addSiteCreated(bikes, now())
 
-	messages := &c.MessageList{}
+	g.addProductCreated(frame, now())
+	g.addProductCreated(wheels, now())
+	g.addProductCreated(seatPost, now())
 
-	addSiteCreated(messages, bikes, now())
+	g.addCustomerCreated(rafal, now())
+	g.addCustomerCreated(yoni, now())
+	g.addCustomerCreated(mike, now())
 
-	addProductCreated(messages, frame, now())
-	addProductCreated(messages, wheels, now())
-	addProductCreated(messages, seatPost, now())
-
-	addCustomerCreated(messages, rafal, now())
-
-	addCartCreated(messages, rafalCart, now())
-
-	addProductAdded(messages, rafalFrame, now())
-	addProductAdded(messages, rafalWheels, now())
-	addProductAdded(messages, rafalSeatPost, now())
+	g.addCartCreated(rafalCart, now())
+	g.addCartCreated(yoniCart, now())
+	g.addCartCreated(mikeCart, now())
 
 	g.Run()
-
-	_, err := post(endpoint, dryRun, messages)
-	if err != nil {
-		return err
-	}
 
 	return nil
 }
 
 func now() string {
 	return time.Now().Format(time.RFC3339)
+	// return time.Now().Format(Format)
 }
 
 func post(endpoint string, dryRun bool, messageList *c.MessageList) (*http.Response, error) {
 	var buf bytes.Buffer
 	marshaler := jsonpb.Marshaler{
-		EmitDefaults: true,
+		EmitDefaults: false,
 		Indent:       "  ",
 	}
 	marshaler.Marshal(&buf, messageList)
@@ -152,76 +151,12 @@ func printEnvVars() {
 	}
 }
 
-func addSiteCreated(m *c.MessageList, site *c.SiteCreated, eventTime string) {
-	m.Message = append(m.Message, &c.Message{
-		SourceId:         "eventgen",
-		EventType:        "site-created",
-		EventTypeVersion: "v1",
-		EventTime:        eventTime,
-		Data: &c.Event{
-			Type:        c.Event_SITE_CREATED,
-			SiteCreated: site,
-		},
-	})
-}
-
-func addCustomerCreated(m *c.MessageList, customer *c.CustomerCreated, eventTime string) {
-	m.Message = append(m.Message, &c.Message{
-		SourceId:         "eventgen",
-		EventType:        "customer-created",
-		EventTypeVersion: "v1",
-		EventTime:        eventTime,
-		Data: &c.Event{
-			Type:            c.Event_CUSTOMER_CREATED,
-			CustomerCreated: customer,
-		},
-	})
-}
-
-func addProductCreated(m *c.MessageList, product *c.ProductCreated, eventTime string) {
-	m.Message = append(m.Message, &c.Message{
-		SourceId:         "eventgen",
-		EventType:        "product-created",
-		EventTypeVersion: "v1",
-		EventTime:        eventTime,
-		Data: &c.Event{
-			Type:           c.Event_PRODUCT_CREATED,
-			ProductCreated: product,
-		},
-	})
-}
-
-func addCartCreated(m *c.MessageList, cart *c.CartCreated, eventTime string) {
-	m.Message = append(m.Message, &c.Message{
-		SourceId:         "eventgen",
-		EventType:        "cart-created",
-		EventTypeVersion: "v1",
-		EventTime:        eventTime,
-		Data: &c.Event{
-			Type:        c.Event_CART_CREATED,
-			CartCreated: cart,
-		},
-	})
-}
-
-func addProductAdded(m *c.MessageList, o *c.ProductAddToCart, eventTime string) {
-	m.Message = append(m.Message, &c.Message{
-		SourceId:         "eventgen",
-		EventType:        "product-add-to-cart",
-		EventTypeVersion: "v1",
-		EventTime:        eventTime,
-		Data: &c.Event{
-			Type:             c.Event_PRODUCT_ADD_TO_CART,
-			ProductAddToCart: o,
-		},
-	})
-}
-
 // Bare object create methods
 
 type generator struct {
 	endpoint           string
 	dryRun             bool
+	tickNumber         int
 	wg                 sync.WaitGroup
 	msgList            *c.MessageList
 	msgMutex           sync.Mutex
@@ -234,13 +169,13 @@ type generator struct {
 	checkouts          map[string]*c.CartSuccessfulCheckout
 }
 
-func NewGenerator(endpoint string, dryRun bool) *generator {
+func NewGenerator(endpoint string, dryRun bool, tickInterval int) *generator {
 	return &generator{
 		endpoint:  endpoint,
 		dryRun:    dryRun,
 		msgList:   &c.MessageList{},
 		timeChan:  time.NewTimer(time.Second).C,
-		tickChan:  time.NewTicker(time.Second).C,
+		tickChan:  time.NewTicker(time.Duration(tickInterval) * time.Second).C,
 		sites:     make(map[string]*c.SiteCreated),
 		customers: make(map[string]*c.CustomerCreated),
 		products:  make(map[string]*c.ProductCreated),
@@ -252,14 +187,18 @@ func NewGenerator(endpoint string, dryRun bool) *generator {
 // Flush posts the
 func (g *generator) Flush() error {
 	if len(g.msgList.Message) == 0 {
+		log.Printf("Deferring flush (no messages)")
 		return nil
 	}
+	log.Printf("Flushing %d messages...", len(g.msgList.Message))
+
 	g.msgMutex.Lock()
 	defer g.msgMutex.Unlock()
 	_, err := post(g.endpoint, g.dryRun, g.msgList)
 	if err != nil {
 		return err
 	}
+	g.msgList.Message = nil
 	return err
 }
 
@@ -270,6 +209,11 @@ func (g *generator) Run() {
 			fmt.Println("Timer expired")
 		case <-g.tickChan:
 			fmt.Println("Ticker ticked")
+			g.tickNumber++
+			g.addSampleProductsToCart()
+			g.deactivateOneCustomer()
+			g.addNewCart()
+			g.Flush()
 		case <-g.doneChan:
 			fmt.Println("Done")
 			return
@@ -279,6 +223,42 @@ func (g *generator) Run() {
 
 func (g *generator) Stop() {
 	g.doneChan <- true
+}
+
+func (g *generator) addNewCart() {
+	id := fmt.Sprintf("User%d", g.tickNumber)
+	site := g.sites["Nice Bikes"]
+	customer := g.createCustomer(id, id+"@example.com")
+	cart := g.createCart(site, customer, "cart"+id)
+	g.addCartCreated(cart, now())
+}
+
+func (g *generator) deactivateOneCustomer() {
+	var junk *c.CustomerCreated
+	for _, customer := range g.customers {
+		if junk == nil && customer.Active {
+			junk = customer
+			junk.Active = false
+			log.Printf("Expired customer %s", junk.CustomerId)
+			return
+		}
+	}
+}
+
+func (g *generator) addSampleProductsToCart() {
+	for _, customer := range g.customers {
+		if !customer.Active {
+			log.Printf("Skipping expired customer %s", customer.CustomerId)
+			continue
+		}
+		cart := g.carts[customer.CustomerId]
+		if cart == nil {
+			panic("Unknown cart for customer " + customer.CustomerId)
+		}
+		for _, product := range g.products {
+			g.addProductToCart(customer, cart, product, now())
+		}
+	}
 }
 
 func (g *generator) createSite(id, desc string) *c.SiteCreated {
@@ -299,12 +279,13 @@ func (g *generator) createProduct(id, desc string) *c.ProductCreated {
 	return item
 }
 
-func (g *generator) createCustomer(id, name, email string) *c.CustomerCreated {
+func (g *generator) createCustomer(id, email string) *c.CustomerCreated {
 	item := &c.CustomerCreated{
 		CustomerId:  id,
 		CustomerUid: id,
-		Name:        name,
+		Name:        id,
 		Email:       email,
+		Active:      true,
 	}
 	g.customers[id] = item
 	return item
@@ -315,7 +296,7 @@ func (g *generator) createCart(site *c.SiteCreated, customer *c.CustomerCreated,
 		CartId:     id,
 		BaseSiteId: site.SiteId,
 	}
-	g.carts[id] = item
+	g.carts[customer.CustomerId] = item
 	return item
 }
 
@@ -336,11 +317,92 @@ func (g *generator) createCartSuccessfulCheckout(
 }
 
 func (g *generator) addProductToCart(customer *c.CustomerCreated, cart *c.CartCreated, product *c.ProductCreated, eventTime string) *c.ProductAddToCart {
-	return &c.ProductAddToCart{
+	item := &c.ProductAddToCart{
 		BaseSiteId: cart.BaseSiteId,
 		CartId:     cart.CartId,
 		UserId:     customer.CustomerId,
 		ProductId:  product.ProductId,
 		EventTime:  eventTime,
 	}
+	g.addProductAdded(item, eventTime)
+	return item
 }
+
+func (g *generator) addSiteCreated(site *c.SiteCreated, eventTime string) {
+	g.addMessage(&c.Message{
+		SourceId:         "eventgen",
+		EventType:        "site-created",
+		EventTypeVersion: "v1",
+		EventTime:        eventTime,
+		Data: &c.Event{
+			Type:        c.Event_SITE_CREATED,
+			SiteCreated: site,
+		},
+	})
+}
+
+func (g *generator) addCustomerCreated(customer *c.CustomerCreated, eventTime string) {
+	g.addMessage(&c.Message{
+		SourceId:         "eventgen",
+		EventType:        "customer-created",
+		EventTypeVersion: "v1",
+		EventTime:        eventTime,
+		Data: &c.Event{
+			Type:            c.Event_CUSTOMER_CREATED,
+			CustomerCreated: customer,
+		},
+	})
+}
+
+func (g *generator) addProductCreated(product *c.ProductCreated, eventTime string) {
+	g.addMessage(&c.Message{
+		SourceId:         "eventgen",
+		EventType:        "product-created",
+		EventTypeVersion: "v1",
+		EventTime:        eventTime,
+		Data: &c.Event{
+			Type:           c.Event_PRODUCT_CREATED,
+			ProductCreated: product,
+		},
+	})
+}
+
+func (g *generator) addCartCreated(cart *c.CartCreated, eventTime string) {
+	g.addMessage(&c.Message{
+		SourceId:         "eventgen",
+		EventType:        "cart-created",
+		EventTypeVersion: "v1",
+		EventTime:        eventTime,
+		Data: &c.Event{
+			Type:        c.Event_CART_CREATED,
+			CartCreated: cart,
+		},
+	})
+}
+
+func (g *generator) addProductAdded(o *c.ProductAddToCart, eventTime string) {
+	g.addMessage(&c.Message{
+		SourceId:         "eventgen",
+		EventType:        "product-add-to-cart",
+		EventTypeVersion: "v1",
+		EventTime:        eventTime,
+		Data: &c.Event{
+			Type:             c.Event_PRODUCT_ADD_TO_CART,
+			ProductAddToCart: o,
+		},
+	})
+}
+
+func (g *generator) addMessage(m *c.Message) {
+	g.msgMutex.Lock()
+	defer g.msgMutex.Unlock()
+	g.msgList.Message = append(g.msgList.Message, m)
+}
+
+// func RandStringBytes(n int) string {
+// 	b := make([]byte, n)
+// 	for i := range b {
+// 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+// 	}
+// 	return string(b)
+// }
